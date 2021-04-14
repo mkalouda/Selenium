4b82b1731fb8b9b6d3f5ca62e3956232b2461978
*mkalouda* <munz.kalouda@gmail.com>
2021-04-13 21:38:25 -0400

    Selenium Class 01

---
+++ src/class01/MaximizeWindow.java
@@ -0,0 +1,18 @@
+package class01;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MaximizeWindow {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("http://facebook.com");
+        driver.manage().window().maximize();
+        //driver.manage().window().fullscreen();
+
+    }
+}
